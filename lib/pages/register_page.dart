ab40ce41d58a5318e4b2b9aca002b0cb787bff69
Lucas Lima <limalucas454@gmail.com>
2021-08-08 15:49:53 -0300

    adding sign_up functionality

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,57 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:login/components/input.dart';
+import 'package:login/pages/sign_up_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class RegisterPage extends StatelessWidget {
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class RegisterPage extends StatefulWidget {
   RegisterPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController _nameInputController = TextEditingController();
+  TextEditingController _mailInputController = TextEditingController();
+  TextEditingController _passwordInputController = TextEditingController();
+  TextEditingController _confirmInputController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Input(
-              label: "Username",
-              icon: Icon(Icons.person),
-              obscured: false,
-            ),
-            Input(
-              label: "Email",
-              icon: Icon(Icons.mail),
-              obscured: false,
-            ),
-            Input(
-              label: "Password",
-              icon: Icon(Icons.vpn_key),
-              obscured: true,
-            ),
-            Input(
-              label: "Confirm Password",
-              icon: Icon(Icons.check),
-              obscured: true,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            MaterialButton(
-              height: 40.0,
-              minWidth: 270.0,
-              color: Theme.of(context).primaryColor,
-              onPressed: () {
-                print('Received click');
-              },
-              child: const Text('Register'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Input(
+                controller: _nameInputController,
+                label: "Username",
+                icon: Icon(Icons.person),
+                obscured: false,
+              ),
+              Input(
+                controller: _mailInputController,
+                label: "Email",
+                icon: Icon(Icons.mail),
+                obscured: false,
+              ),
+              Input(
+                controller: _passwordInputController,
+                label: "Password",
+                icon: Icon(Icons.vpn_key),
+                obscured: true,
+              ),
+              Input(
+                controller: _confirmInputController,
+                validator: (value) {
+                  if (value != _passwordInputController.text) {
+                    return "As senhas devem ser iguais";
+                  }
+                  return null;
+                },
+                label: "Confirm Password",
+                icon: Icon(Icons.check),
+                obscured: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                height: 40.0,
+                minWidth: 270.0,
+                color: Theme.of(context).primaryColor,
+                onPressed: () => _doSignUp(
+                    _formKey, _mailInputController, _passwordInputController),
+                child: const Text('Register'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+void _doSignUp(_formKey, _mailInputController, _passwordInputController) {
+  if (_formKey.currentState.validate()) {
+    SignUpService().signUp(
+      _mailInputController.text,
+      _passwordInputController.text,
+    );
+  } else {
+    print("invalido");
   }
 }
---
+++ lib/pages/sign_up_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:login/routes.dart';
+
+class SignUpService {
+  signUp(String email, String password) async {
+    http.Response response = await http.post(
+      Uri.parse(Routes().signUp()),
+      body: json.encode(
+        {
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        },
+      ),
+    );
+
+    print(response.body);
+  }
+}
