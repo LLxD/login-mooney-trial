6eb1c254f1afbaceee7d7d051bb2cdd1a46bd631
Lucas Lima <limalucas454@gmail.com>
2021-08-08 15:49:40 -0300

    passing controller and validator to input widgets

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -5,33 +5,37 @@ class Input extends StatelessWidget {
       {required this.label,
       required this.icon,
       required this.obscured,
+      this.controller,
+      this.validator,
       Key? key})
       : super(key: key);
 
   final String label;
   final bool obscured;
   final Icon icon;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
-      child: Form(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              obscureText: obscured,
-              decoration: InputDecoration(
-                hintText: label,
-                prefixIcon: icon,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            controller: controller,
+            validator: validator,
+            obscureText: obscured,
+            decoration: InputDecoration(
+              hintText: label,
+              prefixIcon: icon,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
